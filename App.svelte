f04c0e5767e6b41ce6d06924688eb2905c60e2a7
Arsalan Beg Menhaj <arsalan.menhaj@gmail.com>
2022-12-03 09:50:40 -0500

    Initial commit.

---
+++ App.svelte
@@ -0,0 +1,55 @@
+<script>
+  import Interpreter from "./Interpreter.svelte";
+</script>
+
+<link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" />
+
+<style>
+  main {
+    display:flex;
+    flex-direction: row;
+    justify-content: center;
+    color:gray;
+    font-family: Cambria;
+    background-color: black;
+    min-height: 100vh;
+    max-height: fit-content;
+  }
+
+  .main-container {
+    width: 400px;
+  }
+
+  h1 {
+    color: #C3073F;
+    
+  }
+
+  .header-box {
+    display: flex;
+    flex-direction: column;
+    margin-top: 25px;
+    margin-bottom: 25px;
+  }
+
+  span {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
+
+<main>
+  <span class=main-container>
+    <div class=header-box>
+        <h1>BEYONDER</h1>
+        <div>Automatically adds markup for dice rolls, as well as for spell/condition references.</div>
+    </div>
+
+    <div>
+      <Interpreter />
+    </div>
+  </span>
+  
+</main>
+
---
+++ Interpreter.svelte
@@ -0,0 +1,49 @@
+<script>
+  import generateMarkupText from "./mixins/generateMarkupText.js";
+
+  let inputText = "Cantrips (at will): light, sacred flame, thaumaturgy";
+</script>
+
+<style>
+  span {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 50px;
+    margin-bottom: 50px;
+  }
+
+  div {
+    float: left;
+    width: 400px;
+  }
+
+  p {
+    font-size: large;
+    font-weight: 600;
+    color: #C3073F;
+  }
+
+  textarea {
+    height: 200px;
+    width: 400px;
+    background-color: #1A1A1D;
+    color: white;
+  }
+
+  .static {
+    color: gray;
+  }
+</style>
+
+<span>
+  <div>
+    <p>Enter Stat Section here:</p>
+    <textarea bind:value={inputText} />
+  </div>
+  
+  <div>
+    <p>Output:</p>
+    <div class=static><i>{generateMarkupText(inputText) ?? ""}</i></div>
+  </div>
+</span>
